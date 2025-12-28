6a7a4e5479d5394cc9ca0192e3b79b34368fe8da
harryto1 <harry.ruiz6@upr.edu>
2025-12-28 02:39:03 -0400

    feat: integrate Vercel Speed Insights for performance tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,8 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Analytics />
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
